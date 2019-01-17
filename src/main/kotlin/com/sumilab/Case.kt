701bfb2671ee1075f893e4dcd5935860473675f0
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2019-01-17 12:03:21 +0900

    Add Camel, Snake

--- src/main/kotlin/com/sumilab/Case.kt
+++ src/main/kotlin/com/sumilab/Case.kt
@@ -2,24 +2,82 @@ package com.sumilab
 
 object Case {
 
-    const private val separator = '_'
+    private const val separator = '_'
 
     fun toPascal(str: String): String {
-        var newStr = ""
+        return str
+            .split(separator)
+            .trimEmpty()
+            .convertPascal()
+    }
 
-        trimArray(str.split(separator)).forEach {
-            it.forEachIndexed { index, word ->
-                newStr += when (index) {
+    fun toCamel(str: String): String {
+        return str
+            .split(separator)
+            .trimEmpty()
+            .convertCamel()
+    }
+
+    fun toSnake(str: String): String {
+        return str
+            .trimSeparator()
+            .split("")
+            .trimEmpty()
+            .convertSnake()
+
+    }
+
+    private fun List<String>.trimEmpty(): List<String> {
+        return this.filter { it.isNotEmpty() }
+    }
+
+    private fun String.trimSeparator(): String {
+        return this
+            .run {
+                mapIndexed { index, c ->
+                    when {
+                        index == 0 && c == separator -> ""
+                        index == this.lastIndex && c == separator -> ""
+                        else -> c.toString()
+                    }
+                }.joinToString("")
+            }
+    }
+
+    private fun List<String>.convertPascal(): String {
+        return joinToString("") {
+            it.mapIndexed { index, word ->
+                when (index) {
                     0 -> word.toUpperCase()
                     else -> word
                 }
-            }
+            }.joinToString("")
         }
-        return newStr
     }
 
-    private fun trimArray(list: List<String>): List<String> {
-        return list.filter { it.isNotEmpty() }
+    private fun List<String>.convertCamel(): String {
+        return mapIndexed { parentIndex, it ->
+            it.mapIndexed { index, word ->
+                when {
+                    index == 0 && parentIndex == 0 -> word.toLowerCase()
+                    index == 0 -> word.toUpperCase()
+                    else -> word
+                }
+            }.joinToString("")
+        }.joinToString("")
+    }
+
+    private fun List<String>.convertSnake(): String {
+        return mapIndexed { parentIndex, it ->
+            it.mapIndexed { index, word ->
+                when {
+                    index == 0 && parentIndex == 0 -> word.toLowerCase().toString()
+                    word.isUpperCase() -> separator + word.toLowerCase().toString()
+                    else -> word.toString()
+                }
+            }.joinToString("")
+        }.joinToString("")
+
     }
 
 }
--- src/test/kotlin/sumilab/CaseTest.kt
+++ src/test/kotlin/sumilab/CaseTest.kt
@@ -12,5 +12,29 @@ class CaseTest {
         assertEquals(Case.toPascal("hello_world"), pascalCase)
         assertEquals(Case.toPascal("helloWorld_"), pascalCase)
         assertEquals(Case.toPascal("_hello_world_"), pascalCase)
+        assertEquals(Case.toPascal("HelloWorld"), pascalCase)
+        assertEquals(Case.toPascal("helloWorld"), pascalCase)
+    }
+
+    @Test
+    fun testToCamel() {
+        val camelCase = "helloWorld"
+
+        assertEquals(Case.toCamel("hello_world"), camelCase)
+        assertEquals(Case.toCamel("helloWorld_"), camelCase)
+        assertEquals(Case.toCamel("_hello_world_"), camelCase)
+        assertEquals(Case.toCamel("HelloWorld"), camelCase)
+        assertEquals(Case.toCamel("helloWorld"), camelCase)
+    }
+
+    @Test
+    fun testToSnake() {
+        val snakeCase = "hello_world"
+
+        assertEquals(Case.toSnake("hello_world"), snakeCase)
+        assertEquals(Case.toSnake("helloWorld_"), snakeCase)
+        assertEquals(Case.toSnake("_hello_world_"), snakeCase)
+        assertEquals(Case.toSnake("HelloWorld"), snakeCase)
+        assertEquals(Case.toSnake("helloWorld"), snakeCase)
     }
 }
